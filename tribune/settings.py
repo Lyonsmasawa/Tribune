0d666c1a77d4ead6181e8241fcdf187d90c6acc4
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-21 10:06:46 +0300

    test

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', False)
 
 # development
 if config('MODE')=="dev":
